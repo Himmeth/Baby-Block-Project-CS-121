ee367531778986a2715f14661a9c10e0455f5810
leosdmarshall@gmail.com <leosdmarshall@gmail.com>
2019-11-03 18:40:09 -0600

    new sorting process

--- robot_functions.cpp
+++ robot_functions.cpp
@@ -1,10 +1,12 @@
-//#include "robot_functions.cpp"
+#ifndef ROBO_FUNC
+#define ROBO_FUNC
+
+
 #include <iostream>
 #include <iomanip>
 #include <string>
 
-unsigned int move_left_right(unsigned int position, unsigned int movement, bool direction);
-void shift_all(unsigned int position, bool direction, char slots[]);
+/*
 char get_block();
 void print_slots(char slots[]);
 unsigned int put_block(char block, unsigned int position, char array[]);
@@ -14,7 +16,7 @@ unsigned int shift_left(unsigned int position);
 bool robot_ltoreq_slot(char robot, char in_slot);
 char switch_blocks(char robot, unsigned int position, char array[]);
 bool test_empty(unsigned int position, char array[]);
-
+*/
 
 using namespace std;
 
@@ -215,125 +217,4 @@ bool test_empty(unsigned int position, char array[])
 	}
 
 }
-//#endif // !ROBO_FUNC
-
-unsigned int move_left_right(unsigned int position, unsigned int movement, bool direction){
-	if (direction) {
-		for (unsigned int i = 0; i < movement; i++) {
-			position = shift_left(position);
-		}
-		return position;
-	}
-	else{	
-		for (unsigned int i = 0; i < movement; i++) {
-			position = shift_right(position);
-		}
-		return position;
-	}
-}
-
-void shift_all(unsigned int position, bool direction, char slots[]) {
-	char robo_arm;
-	if (direction) {
-		while (position > 1 && !test_empty(position, slots)) {
-			robo_arm = remove_block(position, slots);
-			position = shift_right(position);
-			put_block(robo_arm, position, slots);
-			position -= 2;
-		}
-	}
-	else {
-		while (position < 19 && !test_empty(position, slots)) {
-			robo_arm = remove_block(position, slots);
-			position = shift_left(position);
-			put_block(robo_arm, position, slots);
-			position += 2;
-		}
-	}
-}
-
-
-
-int main() {
-	char robo_arm;
-	char slots[20] = { 0 };
-	bool direction;
-	unsigned int position, blocks_left = 20;
-
-	while (blocks_left != 0) {
-		robo_arm = get_block();
-		position = 0;
-		if (blocks_left == 20) {
-			put_block(robo_arm, 9, slots);
-			blocks_left--;
-		}
-		else if (blocks_left == 19) {
-			direction = robot_ltoreq_slot(robo_arm, slots[9]);
-			if (direction) { //if new block less than one in slot[9] shift to begginning of array and place it there
-				position = move_left_right(9, 9, !direction);
-				put_block(robo_arm, position, slots);
-
-				robo_arm = remove_block(9, slots);
-				position = move_left_right(9, 10, direction); //place block at slot[9] in last slot
-				put_block(robo_arm, position, slots);
-				blocks_left--;
-			}
-			else { //if new block is greater than the one in slot[9] shift it to the end of the array and place it there
-				position = move_left_right(9, 10, direction);
-				put_block(robo_arm, position, slots);
-
-				robo_arm = remove_block(9, slots);
-				position = move_left_right(9, 9, !direction); //place block at slot[9] in first slot
-				put_block(robo_arm, position, slots);
-				blocks_left--;
-			}
-		}
-		else { //actual sorting
-			direction = robot_ltoreq_slot(robo_arm, slots[position]);
-
-			if (direction) { //if held block is less than slot
-				robo_arm = switch_blocks(robo_arm, position, slots);
-				print_slots(slots);
-				position++;
-			}
-			else {
-				position++;
-			}
-
-			unsigned int empty_blocks = 0;
-
-			while (test_empty(position, slots)) { //count number of empty blocks to traverse
-				empty_blocks++;
-				position++;
-			}
-
-			cout << empty_blocks << endl;
-
-			unsigned int starting_shift_position = 0;
-			starting_shift_position = position; //if blocks have to be shifted, it will start at this point
-
-			direction = robot_ltoreq_slot(robo_arm, slots[position]);
-			if (direction) { //if held block less than slot
-				position--;
-				put_block(robo_arm, position, slots);
-				print_slots(slots);
-				blocks_left--;
-			}
-			else if (!direction){//held block is greater than slot
-				bool direction_change = false;
-
-				while (position < 20 && !direction_change) {
-					position++;
-					direction_change = robot_ltoreq_slot(robo_arm, slots[position]);
-				}
-				cout << position << endl;
-				shift_all(starting_shift_position, direction, slots);
-				put_block(robo_arm, --position, slots);
-				print_slots(slots);
-				blocks_left--;				
-			}
-		}
-	}
-
-	print_slots(slots);
-}
+#endif // !ROBO_FUNC
